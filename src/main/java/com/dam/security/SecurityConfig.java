3101260d681175a5e553aacc1a003e2f366c5d47
Sardaukar77 <antonioperezbermejo@hotmail.com>
2025-05-29 09:09:37 +0200

    Cmabiamos seguridad

--- src/main/java/com/dam/security/SecurityConfig.java
+++ src/main/java/com/dam/security/SecurityConfig.java
@@ -47,6 +47,8 @@ public class SecurityConfig {
                 .requestMatchers("/api/desayunos/**").hasRole("ADMIN")
                 .requestMatchers("/uploads/**").permitAll()
                 .requestMatchers("/api/pedidos/**").hasAnyRole("USER", "ADMIN") // Pedidos accesibles a USER y ADMIN
+                .requestMatchers(HttpMethod.PUT, "/api/comandas/*/cancelar").hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/comandas/item/*").hasAnyRole("USER", "ADMIN")
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
